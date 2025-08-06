daf13ec89051e2755e5c7881d88e87b3b22b81fd
Kavindu Methmal <kavindumethmal1234@gmail.com>
2025-08-07 00:50:00 +0530

    Add admin home route and controller with view logic

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AdminController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,3 +17,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/home',[AdminController::class,'index']);
