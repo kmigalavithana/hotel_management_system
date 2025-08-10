44603cd57c75e71317cf6c8d8006f2d6d68bbede
Kavindu Methmal <kavindumethmal1234@gmail.com>
2025-08-10 23:26:23 +0530

    Add admin layout components including sidebar, footer, and dashboard structure.

--- resources/views/admin/css.blade.php
+++ resources/views/admin/css.blade.php
@@ -3,7 +3,7 @@
 <title>Dark Bootstrap Admin </title>
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="robots" content="all,follow">
+<meta name="robots" content="all,folladmin/ow">
 <!-- Bootstrap CSS-->
 <link rel="stylesheet" href="admin/vendor/bootstrap/css/bootstrap.min.css">
 <!-- Font Awesome CSS-->
@@ -11,7 +11,7 @@
 <!-- Custom Font Icons CSS-->
 <link rel="stylesheet" href="admin/css/font.css">
 <!-- Google fonts - Muli-->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
+<link rel="stylesheet" href="admin/https://fonts.googleapis.com/css?family=Muli:300,400,700">
 <!-- theme stylesheet-->
 <link rel="stylesheet" href="admin/css/style.default.css" id="theme-stylesheet">
 <!-- Custom stylesheet - for your changes-->
@@ -19,5 +19,5 @@
 <!-- Favicon-->
 <link rel="shortcut icon" href="admin/img/favicon.ico">
 <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+<script src="admin/https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="admin/https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
